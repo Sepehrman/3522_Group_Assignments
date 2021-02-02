05f52f7e7881a3229bcbe1b87dd434ead761f6c7
Sepehr <sepehrman@icloud.com>
2021-02-02 12:44:24 -0800

    First code

---
+++ Assignments/Assignment1/budget_category.py
@@ -0,0 +1,4 @@
+class BudgetCategory:
+
+    def __init__(self, ):
+        pass
---
+++ Assignments/Assignment1/user.py
@@ -0,0 +1,18 @@
+class User:
+
+    #SHOULD WE HAVE MANY USERS AS A LIST? Which includes parents too?
+    def __init__(self, user_name, user_age, user_type, account_number, bank_name
+                 , balance, budget):
+        self._user_name = user_name
+        self._user_age = user_age
+        self.user_type = user_type
+        self._account_number = account_number
+        self._bank_name = bank_name
+        self._balance = balance
+        self._budget = budget
+
+    @staticmethod
+    def load_test_users(self):
+        return User("Bruce Wayne", 10, "Student", 12345678, "Gotham Bank", 98.32,
+                    40)
+
