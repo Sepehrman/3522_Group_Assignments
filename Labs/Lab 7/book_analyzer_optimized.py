eb27fe417ce9decd4e0e32eb538d475aec29196e
Sam <samerati2000@gmail.com>
2021-03-12 16:34:13 -0800

    Optimized (Runtime: 50ms)

--- Labs/Lab 7/book_analyzer_optimized.py
+++ Labs/Lab 7/book_analyzer_optimized.py
@@ -38,25 +38,8 @@ class BookAnalyzer:
 
         temp_text = []
         for word in self.text:
-            temp_word = word
-            for punctuation in self.COMMON_PUNCTUATION:
-                temp_word = temp_word.replace(punctuation, '')
-            temp_text.append(temp_word)
+            temp_text.append(''.join(list(filter(lambda x: x not in self.COMMON_PUNCTUATION, word))))
         self.text = temp_text
-
-    @staticmethod
-    def is_unique(word, word_list):
-        """
-        Checks to see if the given word appears in the provided sequence.
-        This check is case in-sensitive.
-        :param word: a string
-        :param word_list: a sequence of words
-        :return: True if not found, false otherwise
-        """
-        for a_word in word_list:
-            if word == a_word:
-                return False
-        return True
 
     def find_unique_words(self):
         """
@@ -64,11 +47,11 @@ class BookAnalyzer:
         :return: a list of all the unique words.
         """
         temp_text = self.text
-        # unique_words = []
         while temp_text:
             word = temp_text.pop().lower()
             self.word_list[word] = 1
         return self.word_list
+
 
 
 def main():
